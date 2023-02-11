911913948058e57b760c4ca555bfb7ef9adea86c
kadam-pallavi <117823209+kadam-pallavi@users.noreply.github.com>
2023-02-11 17:24:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+class API{
+  #secure;
+  
+  constructor (url, method='GET'){
+    this.url=url;
+    this.method=method;
+    this.#secure= url.startsWith('https');
+  }
+  isSecure(){
+    return this.#secure;
+  }
+  updateUrl(newURL){
+    this.url=newURL;
+    this.#secure= newURL.startWith('https');
+  }
+}
 
 
 module.exports = { API }
